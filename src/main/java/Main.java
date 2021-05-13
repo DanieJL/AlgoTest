ff5c53ad3c74a2f0d1e1245b62ec49b34542f916
Daniel <danielj.only@gmail.com>
2021-05-13 17:17:35 -0500

    All includise update messages
    Shows all bot's status unified on a time interval instead of spammy individual update messages from each bot.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,36 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Main {
-    public static final int CYCLE_TIME = 15;     //run the market test every X seconds
+    public static final int CYCLE_TIME = 15;       //run the market test every X seconds
+    public static final int UPDATE_CYCLE_TIME = 1; //how many minutes between each discord update (ends up taking longer because kline stuff)
+    private static final DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("hh:mm a");
+    private static final DecimalFormat df = new DecimalFormat("#.###");
+
     public static DiscordBot UPDATER = new DiscordBot();
-    //public static Market MOGUL = new Market();
     public static List<Market> MARKETS = createBotsList();
 
     public static final String botListFile = "src/main/resources/BotList.json";
     public static final boolean persistData = true;
 
+    public static int updateCtr = 1;
     public static void main(String[] args) {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 MARKETS.forEach(Market::runMarketBot);
-                //MOGUL.MarketBot();
+                if(updateCtr<=1){
+                    updates(MARKETS);
+                    updateCtr = ((UPDATE_CYCLE_TIME*60)/CYCLE_TIME);
+                } else {updateCtr--;}
+
             }
-        }, 0, 1000L * CYCLE_TIME); //60000 * MINUTES
+        }, 0, 1000L * CYCLE_TIME);
     }
 
     public static List<Market> createBotsList() {
@@ -41,6 +52,22 @@ public class Main {
             e.printStackTrace();
         }
         return marketBots;
+    }
+
+    private static void updates(List<Market> bots){
+        String d = LocalDateTime.now().format(formatter2);
+        String msg = "```<" + d + ">";
+        for(Market s : bots){
+            msg+= "\n";
+            if(s.getCoinSymbol().equals("")){
+                msg += "[" + s.getName() + "] " + "Searching...";
+            }
+            else{
+                msg += "[" + s.getName() + "] " + s.getCoinSymbol() + " " + df.format(s.getCoinValue()) + " (" + df.format(s.getCoinPercentChange()) + "%)";
+            }
+        }
+        msg += "```";
+        Main.UPDATER.sendUpdateMsg(msg);
     }
 }
 
